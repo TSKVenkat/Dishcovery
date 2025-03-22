d08ff1d33218f57060a6909d176ab5b72282bc0b
Amps <amps2970@gmail.com>
2025-03-22 23:49:23 +0530

    Image features added

--- components/header-auth.tsx
+++ components/header-auth.tsx
@@ -44,7 +44,7 @@ export default async function AuthButton() {
   }
   return user ? (
     <div className="flex items-center gap-4">
-      Hey, {user.email}!
+      Hey, Food lover!
       <form action={signOutAction}>
         <Button type="submit" variant={"outline"}>
           Sign out
